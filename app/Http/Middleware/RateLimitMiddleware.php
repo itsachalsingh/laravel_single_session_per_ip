988b34793cf908491e5090f6c8bf342e923fe043
itsachalsingh <achalchauhan15@gmail.com>
2024-03-31 14:02:30 +0530

    first git

---
+++ app/Http/Middleware/RateLimitMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
+
+class RateLimitMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $ip = $request->ip();
+
+        // Check if there is an active session for the IP address
+        if (Cache::has('session_' . $ip)) {
+            $prompt = 'Another session in progress. Do you want to close the previous session and start a new one?';
+            return response()->json(['message' => $prompt], 403);
+        }
+
+        // If no active session, create a session and proceed
+        Cache::put('session_' . $ip, true, now()->addMinutes(60));
+
+        return $next($request);
+        
+    }
+}
